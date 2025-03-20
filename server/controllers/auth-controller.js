c4bc34f00f7fb994023a4c562e6cc067157dd642
tarunkuma707 <76809697+tarunkuma707@users.noreply.github.com>
2025-03-20 17:55:17 +0530

    Login User Route in Mern Stack Video 14 Thapa Technical

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,5 @@
-const User = require("../models/user-model");
+const User      =   require("../models/user-model");
+const bcrypt    =   require("bcryptjs");
 //////
 /*** 
  * Home Logic
@@ -21,13 +22,35 @@ const register = async (req,res) => {
         if(userExist){
             return res.status(400).json({msg:"Email Already exists"});
         }
-        const userCreated = await User.create({username, email, phone, password });
-        res.status(200).json({ msg:userCreated });
-        
+        // const saltRound     =   10;
+        // const hash_password =   await bcrypt.hash(password,saltRound);
+        const userCreated = await User.create({username, email, phone, password});
+        res.status(200).json({ msg:userCreated, token: await userCreated.generateToken(), userId :userCreated._id.toString() });   
     }
     catch(error){
-        res.status(400).json({ message:error });
+        res.status(500).json({ message:error });
     }    
 }
 
-module.exports = { home, register }
+//// User Login ////////
+const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const userExist = await User.findOne({email});
+        console.log(userExist);
+        if(!userExist){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+        const user = await bcrypt.compare(password, userExist.password);
+        if(user){
+            res.status(200).json({ msg:"Login Success", token: await userExist.generateToken(), userId :userExist._id.toString() });   
+        }
+        else{
+            res.status(401).json({message:"Invalid Email or Password "})
+        }
+    } catch (error) {
+        res.status(500).json({ message:error });
+    }
+}
+
+module.exports = { home, register, login }
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt    =   require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema({
     username:{
         type:String,
@@ -21,6 +23,39 @@ const userSchema = new mongoose.Schema({
         default:false
     },
 });
+
+///////// Password Hash ////////
+userSchema.pre('save', async function(next){
+    console.log("pre method",this);
+    const user = this;
+    if(user.isModified("password")){
+        next();
+    }
+    try {
+        const saltRound     =   await bcrypt.genSalt(10);
+        const hash_password =   await bcrypt.hash(user.password,saltRound);
+        user.password = hash_password;
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+userSchema.methods.generateToken = async function (){
+    try {
+        return jwt.sign({
+            userId: this._id.toString(),
+            email: this.email,
+            isAdmin: this.isAdmin
+        },
+        process.env.JWT_SECRET_KEY,{
+            expiresIn: "30d",
+        }
+        );
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 const User = new mongoose.model("User",userSchema);
 
